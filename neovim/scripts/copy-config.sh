8ec4e7d20ff7508b5583d9b8e0e7f7f27a94899c
Redstonerayy <antonpaulr@protonmail.com>
2023-05-31 10:30:11 +0200

    neovim

--- neovim/scripts/copy-config.sh
+++ neovim/scripts/copy-config.sh
@@ -1,1 +1,1 @@
-cp ../init.vim ~/.config/nvim/init.vim
+cp ./init.vim ~/.config/nvim/init.vim
--- neovim/scripts/setup-config.sh
+++ neovim/scripts/setup-config.sh
@@ -1,7 +1,3 @@
 # make config folders and folder for vim plugins
 mkdir -p ~/.config/nvim
 mkdir -p ~/.nvimplugs
-
-# add sourcing to .*rc
-echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
-echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
